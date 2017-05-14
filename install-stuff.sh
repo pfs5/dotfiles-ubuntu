b032d3cc424e160c8a41209c62ffb9e47580a552
Patrik <patrikfs5@gmail.com>
2017-05-14 22:19:12 +0200

    Initial comommit

---
+++ install-stuff.sh
@@ -0,0 +1,10 @@
+echo '> installing stuff . . .'
+
+# Install fonts
+echo '> installing fonts . . .'
+cp -r .fonts ~/
+
+# i3 configuration
+echo '< configuring i3'
+cp -r i3 ~/.config/
+cp -r .i3blocks ~/
